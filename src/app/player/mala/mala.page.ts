e45dd16f12f591773f32db4c8b8678d2a5da73d8
viktor.hajer <viktor.hajer@evosoft.com>
2020-04-02 14:45:29 +0200

    Mala style update

--- src/app/player/mala/mala.page.ts
+++ src/app/player/mala/mala.page.ts
@@ -2,15 +2,36 @@ import {Component} from '@angular/core';
 import {ModalController} from '@ionic/angular';
 import {NotificationService} from '../../services/notification.service';
 import {ProfileService} from '../../services/profile.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-mala-page',
   templateUrl: './mala.page.html',
+  animations: [
+    trigger('showHide', [
+      state(
+        'a',
+        style({
+          opacity: 1,
+          top: '0px'
+        })
+      ),
+      state(
+        'b',
+        style({
+          opacity: 0,
+          top: '-250px'
+        })
+      ),
+      transition('a <=> b', animate('150ms ease-in-out'))
+    ])
+  ],
   styleUrls: ['./mala.page.scss']
 })
 export class MalaPage {
 
   num = 0;
+  displayFooter = 'a';
 
   constructor(private modalCtrl: ModalController,
               private profileService: ProfileService,
@@ -21,7 +42,14 @@ export class MalaPage {
     this.num = 0;
   }
 
+  onAnimationEvent(event) {
+    if(event.toState === 'b') {
+      this.displayFooter = 'a';
+    }
+  }
+
   pick() {
+    this.displayFooter = 'b';
     this.num++;
     if (this.num === this.profileService.profile.malaBeads) {
       this.num = 0;
